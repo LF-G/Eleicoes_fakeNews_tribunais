47d60b5564f3e10c63e2a2d1c8f59a9637d6f9c5
Luiz Fernando Galati <lfagalati@gmail.com>
2024-05-24 04:04:50 -0300

    Add files via upload

---
+++ escreve_script.py
@@ -0,0 +1,96 @@
+''' Este programa lê as tabelas Excel do projeto "Eleições, Fake News e Os Tribunais:
+desinformação onlina nas eleições de 2018" e constrói um script SQL capaz de adicionar
+essas tabelas (inclusive com o seu conteúdo) em um banco de dados MySQL.           '''
+
+
+import openpyxl
+
+''' Esta função recebe uma string s, substitui todas as aspas (") contidas na string
+pela sequência \" e devolve a nova string s.                                     '''
+
+def process_string (s):
+    if '"' in s:
+        s = s.replace ('"', '\\"')
+
+    return s
+
+
+''' Lê a planilha sheet do arquivo file e escreve no script os trechos que criam
+as tabelas. '''
+
+def create_table (sheet, file):
+    file.write ("CREATE TABLE %s(\n" % sheet.title.lower ())
+    j = 1
+    while (j < sheet.max_column):
+        file.write ("    %s TEXT,\n" % (sheet.cell (1, j).value))
+        j = j + 1
+    file.write ("    %s TEXT);\n\n" % (sheet.cell (1, j).value))
+
+
+''' Lê a planilha sheet do arquivo file e escreve no script os trechos que inserem
+dados nas tabelas. '''
+
+def write_insertions (sheet, file):
+    i = 2
+    while (i <= sheet.max_row):
+        file.write ("INSERT INTO %s (" % (sheet.title.lower ()))
+
+        j = 1;
+        while (j < sheet.max_column):
+            file.write ("%s, " % (sheet.cell (1, j).value))
+            j = j + 1
+        file.write ("%s" % (sheet.cell (1, j).value))
+        file.write (") ")
+
+        file.write ("VALUES (")
+
+        j = 1
+        while (j < sheet.max_column):
+            s = sheet.cell (i, j).value
+            if (type (s) == str):
+                s = process_string (s)
+            file.write ("\"%s\", " % (s))
+            j = j + 1
+        s = sheet.cell (i, j).value
+        if (type (s) == str):
+            s = process_string (s)
+        file.write ("\"%s\"" % (s))
+        file.write (");\n")
+
+        i = i + 1
+
+
+def main ():
+    file = open ("constroi_banco.sql", "x")
+    file.write ("USE fakenews;\n\n")
+
+    book = openpyxl.load_workbook ("[CEPI Fake News 2018] Base de Partes_v.2021.xlsx")
+    sheet = book["Partes"]
+    create_table (sheet, file)
+    write_insertions (sheet, file)
+    file.write ("\n")
+
+    book = openpyxl.load_workbook ("[CEPI Fake News 2018] Base de Processos_v.2021.xlsx")
+    sheet = book["Processos"]
+    create_table (sheet, file)
+    write_insertions (sheet, file)
+    file.write ("\n")
+
+    book = openpyxl.load_workbook ("[CEPI Fake News 2018] Base de Precedentes_v.2021.xlsx")
+    sheet = book["precedentes_TSE"]
+    create_table (sheet, file)
+    write_insertions (sheet, file)
+    file.write ("\n")
+
+    sheet = book["precedentes_TREs"]
+    create_table (sheet, file)
+    write_insertions (sheet, file)
+    file.write ("\n")
+
+    book = openpyxl.load_workbook ("[CEPI Fake News 2018] Base de URLs_v.2021.xlsx")
+    sheet = book["URLs"]
+    create_table (sheet, file)
+    write_insertions (sheet, file)
+    file.write ("\n")
+
+main ()
